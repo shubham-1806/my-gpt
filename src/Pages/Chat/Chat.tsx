19ef23c5f5603125cc19e3766ae4ba5c6c79a136
aerial-ace1 <abhinav.pottigari@gmail.com>
2023-09-27 15:29:16 +0530

    feat: Chat Logic Except RabbitMQ

--- src/Pages/Chat/Chat.tsx
+++ src/Pages/Chat/Chat.tsx
@@ -3,61 +3,86 @@ import style from "./Chat.module.css";
 import upload from "../../assets/upload.svg";
 import enter from "../../assets/enter.svg";
 import { useEffect, useRef, useState } from "react";
+import { useLocation } from "react-router-dom";
 
 interface ChatBubbleProps {
-  type: "user" | "bot";
+  agent: "user" | "bot";
   message: string;
   isUpload: boolean;
   id: string;
 }
 
+
 const Chat = () => {
     const [chatMessages, setChatMessages] = useState<ChatBubbleProps[]>([]);
     const [id, setId] = useState<number>(0);
+    const [loading, setLoading] = useState<boolean>(false);
+    const [botTurn, setBotTurn] = useState<boolean>(false);
     const messageRef = useRef<HTMLInputElement>(null);
     const inputRef = useRef<HTMLInputElement>(null);
+
+    const location = useLocation();
+    const state = location.state;
+
+    useEffect(()=>{
+        if(state){
+            setChatMessages(state);
+            setId(state.length)
+        }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    },[])
 
     const addUserChatBubble = (message: string) => {
         setChatMessages([
             ...chatMessages,
             {
-                type: "user",
+                agent: "user",
                 message: message,
                 isUpload: false,
                 id: id.toString(),
             },
         ]);
-        messageRef.current!.value = "";
-        setId(id + 1);
+    messageRef.current!.value = "";
+    setId(id + 1);
+    setBotTurn(true);
     };
 
-    const addUserChatBubbleOnFileUpload = (filePath:string,fileName:string)=>{
-        console.log(filePath)
+    const addUserChatBubbleOnFileUpload = (
+        filePath: string,
+        fileName: string
+    ) => {
+        console.log(filePath);
         setChatMessages([
             ...chatMessages,
             {
-                type: "user",
+                agent: "user",
                 message: "Uploaded the Document " + fileName,
                 isUpload: true,
                 id: id.toString(),
             },
         ]);
         setId(id + 1);
-    }
-
-    const addBotChatBubble = (message: string) => {
-        setChatMessages([
-            ...chatMessages,
-            {
-                type: "bot",
-                message: message,
-                isUpload: false,
-                id: id.toString(),
-            },
-        ]);
-        setId(id + 1);
+        setBotTurn(true);
     };
-    
+
+    const addBotChatBubble = () => {
+        setLoading(true);
+        setTimeout(() => {
+            setChatMessages([
+                ...chatMessages,
+                {
+                    agent: "bot",
+                    message: "Actual LLM stuff comes here",
+                    isUpload: false,
+                    id: id.toString(),
+                },
+            ]);
+            setId(id + 1);
+            setBotTurn(false);
+            setLoading(false);
+        }, 1000 + Math.random() * 3000);
+    };
+
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
     const uploadFile = (e: any) => {
         const filePath = e.target.files[0].path;
@@ -69,22 +94,68 @@ const Chat = () => {
     useEffect(() => {
         const scrollArray = document.getElementsByClassName("spacerToView");
         const scrollElement = scrollArray[scrollArray.length - 1];
-        scrollElement?.scrollIntoView({behavior: "smooth", block: "center", inline: "center"});
-    },[id]);
+        scrollElement?.scrollIntoView({
+            behavior: "smooth",
+            block: "center",
+            inline: "center",
+        });
+    }, [id]);
+
+    useEffect(() => {
+        if (botTurn)
+            setTimeout(() => {
+                addBotChatBubble();
+            }, 500);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [botTurn]);
 
     return (
         <div className={style.mainContainer}>
             <Header />
             <div className={style.chatWrapper}>
                 <div className={style.chatPart}>
-                    {chatMessages.map((chatMessage) => (
-                        <ChatBubble
-                            type={chatMessage.type}
-                            message={chatMessage.message}
-                            isUpload={chatMessage.isUpload}
-                            key={chatMessage.id.toString()}
-                        />
-                    ))}
+                    {id === 0 ? (
+                        <div className={style.wrapper}>
+                            <div className={style.savedBox}>
+                Effortlessly condense PDFs or web content into concise
+                summaries.
+                            </div>
+                            <div className={style.savedBox}>
+                Interactively inquire about key points from your summaries.
+                            </div>
+                            <div className={style.savedBox}>
+                Access summaries offline, anytime, anywhere.
+                            </div>
+                            <div className={style.savedBox}>
+                Save and revisit your valuable summaries with ease.
+                            </div>
+                        </div>
+                    ) : (
+                        <>
+                            {chatMessages.map((chatMessage) => (
+                                <ChatBubble
+                                    agent={chatMessage.agent}
+                                    message={chatMessage.message}
+                                    isUpload={chatMessage.isUpload}
+                                    key={chatMessage.id.toString()}
+                                    loading={loading}
+                                    id={chatMessage.id}
+                                    currentId={(id - 1).toString()}
+                                />
+                            ))}
+                            {loading && (
+                                <ChatBubble
+                                    agent={"bot"}
+                                    message={"erwerwr"}
+                                    isUpload={false}
+                                    key={(2048).toString()}
+                                    loading={true}
+                                    id={(2048).toString()}
+                                    currentId={(2048).toString()}
+                                />
+                            )}
+                        </>
+                    )}
                 </div>
                 <div className={style.chatPrompt}>
                     <input
@@ -94,7 +165,10 @@ const Chat = () => {
                         ref={inputRef}
                         onChange={(e) => uploadFile(e)}
                     />
-                    <div className={style.uploadButton} onClick={()=>inputRef.current?.click()}>
+                    <div
+                        className={style.uploadButton}
+                        onClick={() => inputRef.current?.click()}
+                    >
                         <img style={{ scale: "0.7" }} src={upload} />
                     </div>
                     <div className={style.input}>
@@ -112,7 +186,7 @@ const Chat = () => {
                             style={{ scale: "0.7", cursor: "pointer" }}
                             src={enter}
                             onClick={() => {
-                                addBotChatBubble(messageRef.current?.value || "");
+                                addUserChatBubble(messageRef.current?.value || "");
                             }}
                         />
                     </div>
