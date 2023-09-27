5b5f0784b44fe2cee971e0bd62c1f0b62803c96f
aerial-ace1 <abhinav.pottigari@gmail.com>
2023-09-27 10:40:27 +0530

    feat: Prompt Box UI

--- src/Pages/Chat/Chat.tsx
+++ src/Pages/Chat/Chat.tsx
@@ -1,5 +1,7 @@
 import { Header } from "../../Components";
 import style from "./Chat.module.css";
+import upload from "../../assets/upload.svg";
+import enter from "../../assets/enter.svg";
 
 
 
@@ -9,6 +11,20 @@ const Chat = () => {
     return (
         <div className={style.mainContainer}>
             <Header />
+            <div className={style.chatWrapper}>
+                <div className={style.chatPart}>
+                    lorem50000000
+                </div>
+                <div className={style.chatPrompt}>
+                    <div className={style.uploadButton}>
+                        <img style={{scale: "0.7"}} src={upload} />
+                    </div>
+                    <div className={style.input}>
+                        <input type="text" placeholder="Type to add a message..." />
+                        <img style={{scale: "0.7"}} src={enter} />
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
